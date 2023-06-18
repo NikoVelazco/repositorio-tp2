4568267d1552b54e7b0c863f41ddf4d8f88804de
AlisonRob <jezitaroberts@gmail.com>
2023-06-17 21:36:52 -0300

    Se crea la implementacion de Mysql para productos

---
+++ tp2_Grupo2/src/main/java/ar/edu/unju/fi/service/imp/ProductoServiceMysqlImp.java
@@ -0,0 +1,58 @@
+package ar.edu.unju.fi.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.entity.Producto;
+import ar.edu.unju.fi.repository.IProductoRepository;
+import ar.edu.unju.fi.service.IProductoService;
+
+@Service("productoServiceMysql")
+public class ProductoServiceMysqlImp implements IProductoService {
+
+	@Autowired
+	private IProductoRepository productoRepository;
+	
+	@Autowired
+	Producto producto;
+	
+	@Override
+	public List<Producto> getListaProducto() {
+		
+		return productoRepository.findByEstado(true);
+	}
+
+	@Override
+	public void guardar(Producto producto) {
+		productoRepository.save(producto);
+	}
+
+	@Override
+	public Producto getBy(Long codigo) {
+		
+		return productoRepository.findById(codigo).get();
+	}
+
+	@Override
+	public void modificar(Producto producto) {
+		productoRepository.save(producto);
+
+	}
+
+	@Override
+	public void eliminar(Producto productoEncontrado) {
+		// eliminacion logica
+		productoEncontrado.setEstado(false);
+		productoRepository.save(producto);
+
+	}
+
+	@Override
+	public Producto getProducto() {
+		
+		return producto;
+	}
+
+}
